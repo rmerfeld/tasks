394342ffc0ff8636c5a547fe0e14038b2063a848
Reinhard Merfeld <reinhard@merfeld.info>
2024-01-10 17:20:46 +0100

    simple rest endpoints and restclient tests

--- src/index.js
+++ src/index.js
@@ -4,4 +4,24 @@ require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(express.json());
+
+// routes
+app.get('/api/v1/tasks', (req, res) => {
+    res.send('get all tasks ...');
+});
+
+app.post('/api/v1/tasks', (req, res) => {
+    res.send(`create task: ${JSON.stringify(req.body)}`);
+});
+
+app.put('/api/v1/tasks/:id', (req, res) => {
+    res.send(`change task with id: ${req.params.id}`);
+});
+
+app.delete('/api/v1/tasks/:id', (req, res) => {
+    res.send(`delete task ${req.params.id}`);
+})
+
 app.listen(port, () => console.log(`Server listening on port ${port} `));
