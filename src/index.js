d3a2bb9526f9bf292ad5b3172c88bf0ca0a5dc11
Reinhard Merfeld <reinhard@merfeld.info>
2024-01-10 19:44:12 +0100

    introduce mongoose

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 require('dotenv').config();
 const tasks = require('./routes/tasks')
+const mongoose = require('mongoose');
 
 const port = process.env.PORT || 5000;
 
@@ -11,5 +12,16 @@ app.use(express.json());
 // routes
 app.use('/api/v1/tasks', tasks);
 
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        app.listen(port, () =>
+            console.log(`Server listening on port ${port} ...`)
+        );
+    } catch (error) {
+        console.log(error);
+    }
+};
 
-app.listen(port, () => console.log(`Server listening on port ${port} `));
+start();
+
