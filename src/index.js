d20cdb8abbeef560ca6a05920e84823beba9d097
Reinhard Merfeld <reinhard@merfeld.info>
2024-01-12 15:22:15 +0100

    route  does not exist - middleware

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ const app = express();
 require('dotenv').config();
 const tasks = require('./route/tasks')
 const mongoose = require('mongoose');
+const notFound = require('./middleware/not-found');
 
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(express.json());
 
+
 // routes
 app.use('/api/v1/tasks', tasks);
+app.use(notFound);
+
 
 const start = async () => {
     try {
